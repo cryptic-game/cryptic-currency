a9d4695ed584be719c07c3f8bb83badb991d91b8
Hendrik <mieasy@pm.me>
2018-10-28 18:50:38 +0100

    added require_session function and updated Pipfile

--- app/basics.py
+++ app/basics.py
@@ -1,5 +1,8 @@
-from flask_restplus import fields
+from flask_restplus import fields, abort
 from flask_restplus.model import Model
+from flask import request
+from functools import wraps
+from requests import get, Response
 from objects import api
 
 ErrorSchema: Model = api.model("Error", {
@@ -9,3 +12,29 @@ ErrorSchema: Model = api.model("Error", {
 SuccessSchema: Model = api.model("Success", {
     "ok": fields.Boolean(readOnly=True)
 })
+
+
+def require_session(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'Token' in request.headers:
+            token: str = request.headers["Token"]
+
+            session: Response = get(api.app.config["auth_api"], headers={
+                "Token": token
+            })
+
+            if session.status_code != 200:
+                try:
+                    msg: str = session.json()["message"]
+                    abort(400, msg)
+                except Exception:
+                    abort(400, "invalid token")
+
+            kwargs["session"] = session.json()
+
+            return f(*args, **kwargs)
+        else:
+            abort(400, "token required")
+
+    return wrapper
