45179d288e71f2b999ff45c4f9d64e181ce28fa4
Chralt98 <chralt98@gmail.com>
2019-03-26 09:48:19 +0100

    Edited error handling.

--- app/resources/wallet.py
+++ app/resources/wallet.py
@@ -9,70 +9,92 @@ def handle(endpoint: list, data: dict) -> dict:
     :param data: source_uuid, key, send_amount, destination_uuid ...
     :return: wallet response for actions
     """
-    # test the casting
-    try:
-        str(data['user_uuid'])
-        str(data['source_uuid'])
-        str(data['key'])
-        int(data['send_amount'])
-        str(data['destination_uuid'])
-        str(data['usage'])
-    except ValueError:
-        return {"wallet_response": "Your input data is in a wrong format!", "user_uuid": "str", "source_uuid": "str",
-                "key": "str", "send_amount": "int", "destination_uuid": "str",
-                "usage": "str"}
-    except KeyError:
-        pass
-    # every time the handle method is called, a new wallet object is created
-    # wallet: Wallet = Wallet()
     db.base.metadata.create_all(bind=db.engine)
     # endpoint[0] will be the action what to do in an array ['get', ...]
     if endpoint[0] == 'create':
+        if 'user_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'user_uuid' has to be set for endpoint create."}}
         try:
-            user_uuid: str = data['user_uuid']
-        except KeyError:
-            return {"wallet_response": "Key 'user_uuid' have to be set for endpoint create."}
+            user_uuid: str = str(data['user_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'user_uuid' has to be string for endpoint create."}}
         wallet_response: dict = Wallet.create(user_uuid)
     elif endpoint[0] == 'get':
+        if 'source_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be set for endpoint get."}}
+        if 'key' not in data:
+            return {"wallet_response": {"error": "Key 'key' has to be set for endpoint get."}}
         try:
-            source_uuid: str = data['source_uuid']
-            key: str = data['key']
-        except KeyError:
-            return {"wallet_response": "Keys 'source_uuid' and 'key' have to be set for endpoint get."}
+            source_uuid: str = str(data['source_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be string for endpoint get."}}
+        try:
+            key: str = str(data['key'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'key' has to be string for endpoint get."}}
         wallet_response: dict = Wallet.get(source_uuid, key)
     elif endpoint[0] == 'send':
+        if 'source_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be set for endpoint send."}}
+        if 'key' not in data:
+            return {"wallet_response": {"error": "Key 'key' has to be set for endpoint send."}}
+        if 'send_amount' not in data:
+            return {"wallet_response": {"error": "Key 'send_amount' has to be set for endpoint send."}}
+        if 'destination_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'destination_uuid' has to be set for endpoint send."}}
+        if 'usage' not in data:
+            usage = ''
+        else:
+            try:
+                usage: str = str(data['usage'])
+            except ValueError:
+                return {"wallet_response": {"error": "Key 'usage' has to be string for endpoint send."}}
         try:
-            usage: str = data['usage']
-        except KeyError:
-            usage: str = ''
+            source_uuid: str = str(data['source_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be string for endpoint send."}}
         try:
-            source_uuid: str = data['source_uuid']
-            key: str = data['key']
-            send_amount: int = data['send_amount']
-            destination_uuid: str = data['destination_uuid']
-        except KeyError:
-            return {"wallet_response": "Keys 'source_uuid' and 'key' and 'send_amount' "
-                                       "and 'destination_uuid' have to be set for endpoint send."
-                                       "You can also use key 'usage' for specify your transfer."}
+            key: str = str(data['key'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'key' has to be string for endpoint send."}}
+        try:
+            send_amount: int = int(data['send_amount'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'send_amount' has to be int for endpoint send."}}
+        try:
+            destination_uuid: str = str(data['destination_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'destination_uuid' has to be string for endpoint send."}}
         wallet_response: dict = Wallet.send_coins(source_uuid, key, send_amount, destination_uuid, usage)
     elif endpoint[0] == 'reset':
+        if 'source_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be set for endpoint reset."}}
         try:
-            source_uuid: str = data['source_uuid']
-        except KeyError:
-            return {"wallet_response": "Key 'source_uuid' has to be set for the endpoint reset."}
+            source_uuid: str = str(data['source_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be string for endpoint reset."}}
         wallet_response: dict = Wallet.reset(source_uuid)
     elif endpoint[0] == 'gift':
+        if 'source_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be set for endpoint gift."}}
+        if 'send_amount' not in data:
+            return {"wallet_response": {"error": "Key 'send_amount' has to be set for endpoint gift."}}
         try:
-            send_amount: int = data['send_amount']
-            source_uuid: str = data['source_uuid']
-        except KeyError:
-            return {"wallet_response": "Keys 'source_uuid' and 'send_amount' have to be set for endpoint gift."}
+            source_uuid: str = str(data['source_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be string for endpoint gift."}}
+        try:
+            send_amount: int = int(data['send_amount'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'send_amount' has to be int for endpoint gift."}}
         wallet_response: dict = Wallet.gift(send_amount, source_uuid)
     elif endpoint[0] == 'delete':
+        if 'source_uuid' not in data:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be set for endpoint delete."}}
         try:
-            source_uuid: str = data['source_uuid']
-        except KeyError:
-            return {"wallet_response": "Key 'source_uuid' has to be set for endpoint delete."}
+            source_uuid: str = str(data['source_uuid'])
+        except ValueError:
+            return {"wallet_response": {"error": "Key 'source_uuid' has to be string for endpoint delete."}}
         wallet_response: dict = Wallet.delete(source_uuid)
     else:
         wallet_response: dict = {"error": "Endpoint is not supported."}
