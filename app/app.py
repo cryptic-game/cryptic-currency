28ddb8446281576b63a2a407843a9aadfc719876
TheCataliasTNT2k <dubistdu2000@gmx.de>
2019-10-13 20:07:15 +0200

    added main.py (#41)

--- app/app.py
+++ app/app.py
@@ -5,10 +5,3 @@ config: Config = get_config()  # / production
 m: MicroService = MicroService("currency")
 
 wrapper: DatabaseWrapper = m.get_wrapper()
-
-if __name__ == "__main__":
-    from resources.wallet import *
-
-    wrapper.Base.metadata.create_all(bind=wrapper.engine)
-
-    m.run()
